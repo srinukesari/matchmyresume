9ec4d85dd09e7b482c78301c0b9077f5cfbc9739
srinukesari <srinukesari333@gmail.com>
2025-06-26 03:31:45 -0400

    added main function with uvicorn

--- backend/main.py
+++ backend/main.py
@@ -69,3 +69,7 @@ async def analyze_resumes(data: AnalysisRequest):
         "suggestions": top_data["suggestions"],
         "results": all_resumes
     }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
